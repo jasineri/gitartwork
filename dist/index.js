da821223c5474fd19ef80d3f767e621bbd59adc6
jasineri <jasineri@gmx.de>
2022-02-19 13:01:27 +0100

    update gitartwork

--- dist/index.js
+++ dist/index.js
@@ -224,7 +224,7 @@ const loadSite_1 = __nccwpck_require__(8155);
 const generateSvg_1 = __nccwpck_require__(6460);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        (0, generateSvg_1.generateSvg)(yield (0, loadSite_1.loadSite)(core.getInput("username")), core.getInput("text"), core.getInput("svg_file_name")
+        (0, generateSvg_1.generateSvg)(yield (0, loadSite_1.loadSite)(core.getInput("user_name")), core.getInput("text"), core.getInput("svg_file_name")
         // await loadSite("jasineri"),
         // "JASINERI",
         // "gitartwork.svg"
